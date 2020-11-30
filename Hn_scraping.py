c96f1275d1df778f51450a127194bf662722d32f
Adewale Adeleye <Adewagold@yahoo.com>
2020-12-01 00:02:11 +0100

    Add Hacker news web-scraping

--- Hn_scraping.py
+++ Hn_scraping.py
@@ -10,3 +10,14 @@ votes= soup.select('.score')
 print(links[0])
 print(votes[0])
 print(votes[0].get("id"))
+
+
+def create_custom_hacker_news(links, votes):
+    hn=[]
+    for index, item in enumerate(links):
+        title = links[index].getText
+        hn.append(title)
+    return hn
+
+
+print(create_custom_hacker_news(links, votes))
